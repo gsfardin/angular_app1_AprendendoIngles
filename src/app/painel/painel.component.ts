d203991465a64d1c6f2b1914a04c8561e16784fd
Gabriel Fardin <gsfardin@gmail.com>
2020-12-06 15:31:02 -0300

    Utilização de event binding para recuperar os dados inseridos pelo usuário no template

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,12 +11,20 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = "Traduza a frase:"
   public frases: Frase[] = FRASES
+  public resposta: string
 
   constructor() {
     console.log(this.frases)
    }
 
   ngOnInit(): void {
+  }
+
+  // atualizaResposta(): void --> Declarar o método sem o operador de visibilidade, assume puclic.
+  public atualizaResposta(resposta: Event): void {
+    // <HTMLInputElement> Declara que o atributo é um elemento HTML, permite buscar o estado do elemento no DOM.
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
   }
 
 }
