faa5c2986c3189ba7b3efcae0e7fb8e20fad2af8
Gabriel Fardin <gsfardin@gmail.com>
2020-12-06 14:39:24 -0300

    Criação da classe compartilhada Frase, arquivo mock para teste e ajuste na classe do componente painel.component para testes com o mock

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Array<Frase> = [
+    { fraseIng: 'I like to learn', frasePtbr: 'Eu gosto de aprender' },
+    { fraseIng: 'I watch tv', frasePtbr: 'Eu assisto tv' },
+    { fraseIng: 'How are you?', frasePtbr: 'Como vai você?' },
+    { fraseIng: 'I eat bread', frasePtbr: 'Eu como pão' }
+];
---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,15 @@
+export class Frase {
+
+    /*
+    public fraseIng: string;
+    public farasePtbr: string;
+
+    constructor(fraseIng: string, frasePtbr: string) {
+        this.fraseIng = fraseIng;
+        this.frasePtbr = frasePtbr;
+    }
+    */
+
+    // Construção do construtor de forma menos verbosa.
+    constructor(public fraseIng: string, public frasePtbr: string) {}
+}
